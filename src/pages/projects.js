e29c6fe5c149615ccd4b8a2cbe408c4e501b1b35
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 11:00:17 +0000

    Add seo to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const Projects = () => (
   <div>
-    <SEO title="Home" />
+    <SEO title="Projects" />
     <div className="navigation"></div>
     <div className="main"></div>
     <div className="footer"></div>
