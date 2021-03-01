5a81ad2a1ba2353b0da7bae52351e2d596f0a42b
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 09:51:17 +0000

    Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <div>
     <SEO title="Home" />
-    <h1 className="w-full bg-gray-900 h-screen flex justify-between" >Hi people</h1>
-    <p className="font-medium bg-gray-900 text-gray-600 text-2xl" >Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 className="">Hi people</h1>
+    <p className="" >Welcome to your new Gatsby site.</p>
   </div>
 )
 
