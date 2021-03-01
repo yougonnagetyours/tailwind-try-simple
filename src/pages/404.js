664d61f350b08303dc7dcfcfc04634ca53665135
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 10:48:08 +0000

    fic bug with missing Layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,11 @@ import React from "react"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <div>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </div>
 )
 
 export default NotFoundPage
