780e6c9d824344d160b9c746b566fe6db1749348
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 09:33:37 +0000

    Fix bugs - delete <Layout>

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" />
     <h1 className="w-full bg-gray-900 h-screen flex justify-between" >Hi people</h1>
     <p className="font-medium text-gray-600 text-2xl" >Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
