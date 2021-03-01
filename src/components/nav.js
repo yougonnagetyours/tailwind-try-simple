11ef9794da725301551882e1f609f810bee9767d
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 11:49:16 +0000

    add brand and Links to li's

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,8 +5,10 @@ import React from "react"
 const Nav = () => (
   <nav>
       <ul>
-          <Link to="home">Home</Link>
-          <Link to="projects">Projekty</Link>
+          <div>Brand</div>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="projects">Projekty</Link></li>
+          <li><Link to="/">Kontact</Link></li>
       </ul>
   </nav>
 )
