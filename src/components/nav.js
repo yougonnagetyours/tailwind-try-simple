27b8adf72ea89f78068c4b10460e911a7508a702
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 12:36:30 +0000

    Add link value to <p>

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ const Nav = () => (
   <nav>
       <ul>
           <div>Brand</div>
-          <li><Link to="/">Home</Link></li>
+          <li><Link to="/"><p>Home</p></Link></li>
           <li><Link to="projects">Projekty</Link></li>
           <li><Link to="/">Kontakt</Link></li>
       </ul>
@@ -15,3 +15,4 @@ const Nav = () => (
 )
 
 export default Nav
+
