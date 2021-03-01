b233a6d439c8172bfe0e3f168ad22e6725f10ea6
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-01 09:31:46 +0000

    Add tailwind issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+//import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1 className="w-full bg-gray-900 h-screen flex justify-between" >Hi people</h1>
+    <p className="font-medium text-gray-600 text-2xl" >Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
