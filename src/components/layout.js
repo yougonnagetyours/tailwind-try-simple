92577541b79c9712f035f0a84d98c428fee048c8
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-03-08 10:09:15 +0000

    reset layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,15 +28,15 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: `0`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: `0`,
         }}
       >
         <main>{children}</main>
         <footer
           style={{
-            marginTop: `2rem`,
+            marginTop: `0`,
           }}
         >
           © {new Date().getFullYear()}, Built with
